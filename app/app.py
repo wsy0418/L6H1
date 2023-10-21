46203c1743d7eca6c03320fdd0567f936fece8ec
wsy0418 <146824307+wsy0418@users.noreply.github.com>
2023-10-21 21:21:07 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -19,5 +19,5 @@ def hello_world():
         predictions = model.predict([np_arr])  
         predictions_to_str = str(predictions)
         #return predictions_to_str
-        return render_template('index.html', href2='The suitable bag for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
+        return render_template('index.html', href2='The suitable watch for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
 
